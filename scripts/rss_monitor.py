cd9d511de955b81ac4aaba86784781e5c199f6de
videsouza <139879885+videsouza@users.noreply.github.com>
2026-05-26 11:42:50 -0300

    Update rss_monitor.py

--- scripts/rss_monitor.py
+++ scripts/rss_monitor.py
@@ -1,8 +1,37 @@
+import os
 import feedparser
+from supabase import create_client
+from dateutil import parser
+
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 rss_url = "https://revista.ibict.br/ciinf/gateway/plugin/WebFeedGatewayPlugin/rss2"
 
 feed = feedparser.parse(rss_url)
 
 for entry in feed.entries[:5]:
-    print(entry.title)
+
+    titulo = entry.title
+    link = entry.link
+
+    publicado = None
+
+    if hasattr(entry, "published"):
+        publicado = parser.parse(entry.published).isoformat()
+
+    artigo = {
+        "titulo": titulo,
+        "link": link,
+        "revista": "Ciência da Informação",
+        "publicado_em": publicado
+    }
+
+    response = supabase.table("articles").upsert(
+        artigo,
+        on_conflict="link"
+    ).execute()
+
+    print(f"Salvo: {titulo}")
