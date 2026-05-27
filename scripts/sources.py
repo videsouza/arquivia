4e94316ca948311871b4d2e1db427ae4c1c5f5dc
videsouza <139879885+videsouza@users.noreply.github.com>
2026-05-27 10:28:10 -0300

    Create sources.py

---
+++ scripts/sources.py
@@ -0,0 +1,55 @@
+FONTES = [
+
+    # =========================================
+    # CIÊNCIA DA INFORMAÇÃO
+    # =========================================
+
+    {
+        "revista": "Perspectivas em Ciência da Informação",
+        "area": "Ciência da Informação",
+        "rss": "https://periodicos.ufmg.br/index.php/pci/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+
+    {
+        "revista": "Informação & Sociedade",
+        "area": "Ciência da Informação",
+        "rss": "https://periodicos.ufpb.br/index.php/ies/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+    {
+        "revista": "Encontros Bibli",
+        "area": "Biblioteconomia",
+        "rss": "https://periodicos.ufsc.br/index.php/eb/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+    
+    {
+        "revista": "Transinformação",
+        "area": "Biblioteconomia",
+        "rss": "https://periodicos.puc-campinas.edu.br/transinfo/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+    # =========================================
+    # ARQUIVOLOGIA
+    # =========================================
+
+    {
+        "revista": "Acervo",
+        "area": "Arquivologia",
+        "rss": "https://revista.an.gov.br/index.php/revistaacervo/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+    {
+        "revista": "Ágora",
+        "area": "Arquivologia",
+        "rss": "https://agora.emnuvens.com.br/ra/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    },
+
+    {
+        "revista": "Archeion Online",
+        "area": "Arquivologia",
+        "rss": "https://periodicos.ufpb.br/index.php/archeion/gateway/plugin/WebFeedGatewayPlugin/rss2"
+    }
+
+]
